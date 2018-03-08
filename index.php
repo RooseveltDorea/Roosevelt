4875d40ae58b5a375e4ac798212745dc340523a9
RooseveltDorea <rooseveltdorea@hotmail.com>
2018-03-08 10:20:06 -0300

    Modificações no index.php, Cor

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+echo "Meu Primeiro Código PHP! <Strong><b><font color='#FF0000'>Olá Mundo</Strong></font></b><br/>"
+;
+echo "Vamos entender o que é cliente e Servidor<br />";
+echo " <color='DFDFDF'> Eu sou o usuário (request) uma solicitação para o servidor";
+echo "<b><font color='#FF0000'> Status </font></b>"
+?>

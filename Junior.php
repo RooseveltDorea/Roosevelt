8ee897b19dacfc7ee93f0e007c421c7d3056444b
RooseveltDorea <rooseveltdorea@hotmail.com>
2018-03-08 17:01:00 -0300

    Add Junior.php

---
+++ Junior.php
@@ -0,0 +1,11 @@
+<?php
+
+Echo "Git e github course";<br/>
+
+Echo "Esse é um repositório teste para esnsinar como o Git funciona.";
+
+Echo "Saiba mais sobre Git, procurando no [FESCETE](http://sistescom.com.br/fescete22)";
+
+Echo "Escrito por Roosevelt.";
+
+?>

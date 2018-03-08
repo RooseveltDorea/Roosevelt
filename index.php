d24df4dc5ebb979577ec66eec94664713709049a
RooseveltDorea <rooseveltdorea@hotmail.com>
2018-03-08 10:45:11 -0300

    novas linha

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ echo "Meu Primeiro Código PHP! <Strong><b><font color='#FF0000'>Olá Mundo</Str
 echo "Vamos entender o que é cliente e Servidor<br />";
 echo " <color='DFDFDF'> Eu sou o usuário (request) uma solicitação para o servidor";
 echo "<b><font color='#FF0000'> Status </font></b><br/>";
-Echo "Mais uma linha Add";
+Echo "Mais uma linha Add <br/>";
+echo "Outra linha para teste de Diff";
 ?>

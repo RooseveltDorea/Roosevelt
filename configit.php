b3b29f8a68558beed8bf31c35b02a5e7258e1744
RooseveltDorea <rooseveltdorea@hotmail.com>
2018-03-08 18:08:10 -0300

    configit.php

---
+++ configit.php
@@ -0,0 +1,23 @@
+<?php 
+//-<b><font color='black'>TEXTO</font></b><br/>
+echo "Olá vamos configurar um Repositório e <Strong>enviar arquivos<Strong/> <br/>";
+echo "1º) - Escolha um local no seu computador, Exemplo: <strong> C:\xampp\htdocs<strong><br/>";
+echo "2º) -<b><font color='#FF0000'>Por que escolhi esse diretório(local)?</font></b><br/>";
+echo "-<b><font color='Blue'>Resposta: Minhas aplicações web em PHP, ficam armazenadas dentro da pasta xampp\htdocs, então tudo que for colocado nesse diretório, será enviado para o repositório garantindo a segurança da versão. </font></b><br/>";
+echo "<br/>";
+echo "-<b><font color='Green'>Vamos aos Comandos!</font></b><br/>";
+echo "-<b><font color='black'>Para criar uma pasta no sublime, use:<br/> Mkdir</font></b><br/>";
+echo "-<b><font color='black'>Para entrar na pasta use: <br/> cd (nome da pasta)</font></b><br/>";
+
+echo "-<b><font color='black'>Para add um arquivo use:<br/><br/>$ git add (nome do arquivo).(extensão) 
+<br/>-Exemplo: git add index.php
+<br/>-Comitar: git commit -m (nome do arquivo entre parenteses)
+<br/>Comando git status
+<br/>Comando # Vim # serve para criar um documeno(arquivo) ou editá-lo
+<br/>Comando git log
+<br/>Comando git shortlog -sn
+<br/>Comando git log --graph<br></font></b><br/>";
+
+
+?>
+
